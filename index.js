306920634421fad1988b8c26d4f61207b64a0bb0
Renan Benatti Dias <renanbenattidias@MacBook-Pro-de-Renan.local>
2018-09-04 20:54:59 -0400

    Testing stuff

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const socket = require('socket.io');
 const logger = require('morgan');
@@ -8,6 +9,8 @@ const app = express();
 const server = app.listen(8080, function() {
 	console.log("Listening to request on 8080");
 });
+
+const shouldPrintDebug = process.env.ENVIROMENT == 'development';
 
 // Static files
 app.use(express.static('public'));
@@ -22,20 +25,25 @@ const io = socket(server);
 
 io.on('connection', socket => {
 	socket.on('room', routePath => {
-		console.log("Did enter " + routePath);
+		if(shouldPrintDebug) console.log("Did enter room: " + routePath);
 		socket.join(routePath);
 	});
 
 	socket.on('chat', function(data) {
 		const path = data.pathName;
-		console.log("Did send on chat " + path);
+		if(shouldPrintDebug) console.log("User " + data.userName + " did send message");
 		socket.to(path).emit('chat', data)
 	});
 
 	socket.on('typing', function(data) {
-		const path = data.pathName;	
+		const path = data.pathName;
+		if(shouldPrintDebug) console.log("User " + data.userName + " is typing");
 		socket.to(path).broadcast.emit('typing', data);
 	});
+
+	// socket.on('clearFeedback', path => {
+	// 	socket.to(path).broadcast.emit('clearFeedback');
+	// });
 });
 
 app.use('*', function(request, response) {
